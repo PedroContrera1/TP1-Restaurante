3642bc01c0bea7ea7876b6b48bbdc16c393f9245
PedroContrera1 <pedrocontrera1745@gmail.com>
2026-03-19 17:12:48 -0300

    Commit Restaurante

---
+++ src/main/java/Exceptions/MesaException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class MesaException extends RuntimeException {
+    public MesaException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/Exceptions/PedidoException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class PedidoException extends RuntimeException {
+    public PedidoException(String message) {
+        super(message);
+    }
+}
